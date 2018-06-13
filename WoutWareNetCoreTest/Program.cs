a8a009275cae647266c481873cab908c40dd4e71
Stefan.Steiger <me@spamhaus.com>
2018-06-13 17:30:59 +0200

    Projektdateien hinzufügen.

---
+++ WoutWareNetCoreTest/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WoutWareNetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WW.WWLicense.SetLicense("AQAAAG8k3LVHqvLi1bJYXpQFgmtG/MRWYZcdrTG2MlOUb1E3Pe/4mkhuVSyJ6eXKeGH5jY9kSEBa94uYpww4TBqiLMFVTgOI9z5+ZRTZJ5szezpYXlMvpVuAmXb2EWGMUYcdWKYP0WMTGDTStjaKqQZGA4f0rpKYak0D1/n/6GWpPYEp");
+            
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
